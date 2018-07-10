5ac6bdf9ed2bb5129145a24ae85889c3ac6c259e
David <40622672+macroTbeast@users.noreply.github.com>
2018-07-10 19:35:41 +0200

    Login & register buttons
    https://getbootstrap.com/docs/4.0/components/

--- resources/views/web-pages/index.blade.php
+++ resources/views/web-pages/index.blade.php
@@ -1,5 +1,12 @@
 @extends('layouts.app')
 
 @section('content')
-    <p>Index</p>
-@endsection
+<div class="jumbotron">
+<center>
+    <h1 class="display-4">Hello, there friend!</h1>
+    <p class="lead">Welcome to my test forum/blog. Feel free to create an account if you want to post something, or just have a look around as a guest.</p>
+    <hr class="my-4">
+    <p><a class="btn btn-primary btn-lg" href="/login" role="button">Login</a> <a class="btn btn-success btn-lg" href="/register" role="button">Register</a></p>
+</center>
+</div>
+    @endsection
